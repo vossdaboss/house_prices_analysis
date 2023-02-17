ec4c8dbec60f52b5e7d378988e05d20c6972ceb3
Justus Voss <justusvoss27@gmail.com>
2023-02-16 22:08:25 -0800

    Initial Commit

---
+++ housing_data.py
@@ -0,0 +1,10 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import spearmanr
+
+import os
+for dirname, _, filenames in os.walk('data_archive'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
